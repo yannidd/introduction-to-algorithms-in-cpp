ab8efe2dce04e08c496fc29f69005d7cac7178c0
yannidd <yanislavdonchev@gmail.com>
2019-09-18 23:29:48 +0100

    added bubble sort

---
+++ sorting/bubble_sort.h
@@ -0,0 +1,38 @@
+#ifndef SORTING_BUBBLE_SORT_H_
+#define SORTING_BUBBLE_SORT_H_
+
+#include <vector>
+#include <iostream>
+
+
+template<class T>
+void BubbleSort(std::vector<T> &A, bool order) {
+  size_t length = A.size();
+
+  switch (order) {
+    case true:
+      
+      for (size_t i = 0; i < length - 1; i++) {
+        for (size_t j = 0; j < (length - i - 1); j++) {
+          if (A[j] > A[j + 1]) {
+            std::swap(A[j], A[j + 1]);
+          }
+        }
+      }
+
+      break;
+    case false:
+
+      for (size_t i = 0; i < length - 1; i++) {
+        for (size_t j = 0; j < (length - i - 1); j++) {
+          if (A[j] < A[j + 1]) {
+            std::swap(A[j], A[j + 1]);
+          }
+        }
+      }
+
+      break;
+  }
+}
+
+#endif // SORTING_BUBBLE_SORT_H_
--- tests/sorting_test.cc
+++ tests/sorting_test.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+#include "sorting/bubble_sort.h"
 #include "sorting/insertion_sort.h"
 #include "sorting/selection_sort.h"
 #include "sorting/merge_sort.h"
@@ -8,6 +9,20 @@
 #include "gtest/gtest.h"
 
 namespace {
+	TEST(BubbleSortTest, Ascending) {
+		std::vector<int> A = { 1, 2, 3, 4, 5, 6 };
+		std::vector<int> B = { 5, 2, 4, 6, 1, 3 };
+		BubbleSort(B, true);
+		EXPECT_EQ(A, B);
+	}
+
+	TEST(BubbleSortTest, Descending) {
+		std::vector<int> A = { 6, 5, 4, 3, 2, 1 };
+		std::vector<int> B = { 5, 2, 4, 6, 1, 3 };
+		BubbleSort(B, false);
+		EXPECT_EQ(A, B);
+	}
+
 	TEST(InsertionSortTest, Ascending) {
 		std::vector<int> A = { 1, 2, 3, 4, 5, 6 };
 		std::vector<int> B = { 5, 2, 4, 6, 1, 3 };
